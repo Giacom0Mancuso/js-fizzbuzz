bb53a08bbf1f8231a7936fa3e99f96961ef511a7
Giacom0Mancuso <giacomomancuso35@email.com>
2023-11-15 15:36:27 +0100

    made index and js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+let Num100 = [100];
+
+for (let i = 0; i <= Num100.length; i++) {
+    if(i % 3 == 0){
+        if(i % 5 == 0){
+            nuovoVincente = "FizzBuzz";
+            Num100.push(nuovoVincente);
+            }
+    else{
+        let nuovoVincente = "fizz";
+        Num100.push(nuovoVincente);
+    }
+    }
+    if(i % 5 == 0){
+    nuovoVincente = "buzz";
+    Num100.push(nuovoVincente);
+    }
+    else{
+    nuovoVincente = i;
+    Num100.push(nuovoVincente);
+    }
+}
+
+
+for (let i = 0; i < Num100.length; i++) {
+    console.log(Num100[i]);
+}
